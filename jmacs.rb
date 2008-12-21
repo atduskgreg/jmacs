6781145e02921731e93d7f4928a3f3c754d402f0
Greg Borenstein <greg@mfdz.com>
2008-12-21 15:50:10 -0800

    refactoring specs and init file

--- jmacs.rb
+++ jmacs.rb
@@ -1,13 +1,4 @@
-require "rubygems"
-require 'sinatra'
-require 'json'
-
-APP_ROOT = File.expand_path(File.dirname(__FILE__)) unless defined? APP_ROOT
-
-
-require "#{APP_ROOT}/models/init_from_path.rb"
-require "#{APP_ROOT}/models/document.rb"
-require "#{APP_ROOT}/models/directory.rb"
+require 'init.rb'
 
 helpers do
   def save_document( params )
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,2 @@
-require 'rubygems'
+require File.expand_path(File.dirname(__FILE__)) + '/../init.rb'
 require 'spec'
-
-APP_ROOT = File.expand_path(File.dirname(__FILE__)) + '/../' unless defined? APP_ROOT
-
-
-require "#{APP_ROOT}/models/init_from_path.rb"
-require "#{APP_ROOT}/models/document.rb"
-require "#{APP_ROOT}/models/directory.rb"
