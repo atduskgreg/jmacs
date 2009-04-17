f232a2d21cc6fdf82dd094c8255ca7e362d1d14b
Greg Borenstein <greg@mfdz.com>
2009-04-17 12:08:25 -0700

    ditch init file since it doesn't seem to take

--- init.rb
+++
@@ -1,9 +0,0 @@
-require "rubygems"
-require 'sinatra'
-require 'json'
-
-APP_ROOT = File.expand_path(File.dirname(__FILE__)) unless defined? APP_ROOT
-
-require "#{APP_ROOT}/models/init_from_path.rb"
-require "#{APP_ROOT}/models/document.rb"
-require "#{APP_ROOT}/models/directory.rb"
--- jmacs.rb
+++ jmacs.rb
@@ -1,4 +1,12 @@
-require 'init.rb'
+require "rubygems"
+require 'sinatra'
+require 'json'
+
+APP_ROOT = File.expand_path(File.dirname(__FILE__)) unless defined? APP_ROOT
+
+require "#{APP_ROOT}/models/init_from_path.rb"
+require "#{APP_ROOT}/models/document.rb"
+require "#{APP_ROOT}/models/directory.rb"
 
 helpers do
   def save_document( params )
