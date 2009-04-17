afa83c1fca23c60e347bfd1524dde2a2c12fc4a1
Greg Borenstein <greg@mfdz.com>
2009-04-17 12:19:51 -0700

    make spec_helper aware of the removal of init.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,8 @@
-require File.expand_path(File.dirname(__FILE__)) + '/../init.rb'
+require "rubygems"
+require 'sinatra'
+require 'json'
+
+require File.expand_path(File.dirname(__FILE__)) + "/../models/init_from_path.rb"
+require File.expand_path(File.dirname(__FILE__)) + "/../models/document.rb"
+require File.expand_path(File.dirname(__FILE__)) + "/../models/directory.rb"
 require 'spec'
